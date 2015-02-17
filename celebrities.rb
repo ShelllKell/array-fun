08e07be93cfc0f82fa85e58074f790934eb020e6
Shelby Kelly <shelby.kelly@colorado.edu>
2015-02-17 10:07:10 -0700

    shelby's answers

--- celebrities.rb
+++ celebrities.rb
@@ -3,42 +3,73 @@ require_relative "testing_library"
 celebs = ["Britney", "Miley", "Madonna", "Cher", "Bruno", "Michael"]
 
 def how_many_celebrities(celebs)
-
+  celebs.length
 end
 
 
 def name_lengths(celebs)
-
+  celebs.map do |c|
+    c.length
+  end
 end
 
 
 def reverse_celebrity_names(celebs)
-
+  celebs.map do |c|
+    c.reverse
+  end
 end
 
 
 def first_three_celebrities_with_each(celebs)
+  first_three_celebs = []
+  counter = 0
 
+  celebs.each do |celeb|
+    first_three_celebs << celeb
+    counter = counter + 1
+
+    if counter == 3
+      break
+    end
+
+  end
+
+  first_three_celebs
 end
 
 
 def first_three_celebrities_without_each(celebs)
-
+  celebs[0..2]
 end
 
 
 def reverse_case_celebrities_names(celebs)
-
+  array = []
+  celebs.map do |c|
+    array << c.swapcase
+  end
+  array
 end
 
 
 def sum_of_all_celebrity_name_lengths(celebs)
-
+  celebs.join.length
 end
 
 
 def celebrities_with_long_names(celebs)
+  array = []
 
+  celebs.each do |celeb_name|
+    if celeb_name.length > 5
+      array << true
+    else
+      array << false
+    end
+  end
+
+  array
 end
 
 
